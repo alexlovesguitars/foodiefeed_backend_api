0b13be04cd4008eaa7898a59323e7743000af112
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2026-03-09 13:04:17 +0100

    Updated User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
+
   has_one_attached :avatar
+
   has_many :recipes, dependent: :destroy
   has_many :cookbooks, dependent: :destroy
   has_many :messages
   has_many :conversations, through: :messages
 
+  enum :role, {
+    user: 0,
+    creator: 1,
+    admin: 2
+  }
 
-
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- db/migrate/20260308115355_create_users.rb
+++ db/migrate/20260308115355_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
+      t.string :username
       t.string :email
       t.string :password_digest
+      t.integer :role, default: 0, null: false
 
       t.timestamps
     end
